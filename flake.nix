7d6d99ce7279641e51d03bb0efd601b55646732c
Michalis Pardalos <me@mpardalos.com>
2025-05-27 17:59:37 +0100

    Set up nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A REPL for Verilog expressions";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.iverilog
+            (pkgs.python3.withPackages (ps: with ps; []))
+          ];
+        };
+      }
+    );
+}
